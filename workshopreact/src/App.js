41b4f3d21e05ef82e04a2ca5022ff8d4a1462c0a
Hugo Baret <hugo.baret@epitech.eu>
2021-02-10 15:16:38 +0400

    2

--- workshopreact/src/App.js
+++ workshopreact/src/App.js
@@ -1,8 +1,19 @@
+import Hello from './Hello';
+
 function App() {
   return (
-        <p>
-          hello world
-        </p>
+    <div class="navbar-fixed">
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#!" class="brand-logo">LOGO</a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="sass.html">Home</a></li>
+          <li><a href="badges.html">Menu</a></li>
+          <li><a href="badges.html">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
   );
 }
 
---
+++ workshopreact/src/Hello.js
@@ -0,0 +1,8 @@
+const Hello = () => {
+	return (
+		<h1>
+			Home Menu Contact
+		</h1>
+	);
+}
+export default Hello;
